06fe3f6a3966c61312aa35f133dd1ddefe08d92c
Kirill Kamakin <k.kamakin@protonmail.ch>
2021-12-13 10:04:05 +0300

    Implement face detection image capture callback

---
+++ sdk/src/main/java/com/example/testapp/FaceDetectionImageCaptureCallback.kt
@@ -0,0 +1,16 @@
+package com.example.testapp
+
+import com.google.mlkit.vision.common.InputImage
+import com.google.mlkit.vision.face.Face
+import com.google.mlkit.vision.face.FaceDetector
+import timber.log.Timber
+
+class FaceDetectionImageCaptureCallback(
+    private val faceDetector: FaceDetector,
+) : BaseImageCaptureCallback<List<Face>>() {
+
+    override suspend fun processImage(image: InputImage): Result<List<Face>> {
+        Timber.v("processImage() called with: image = $image")
+        return runCatching { faceDetector.process(image).await() }
+    }
+}
